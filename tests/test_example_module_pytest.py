da18675c9058b912080b65dc7a7d8d31276b686b
jobjørn_myren <111862960+swampbear@users.noreply.github.com>
2025-03-10 10:29:45 +0100

    Initial commit

---
+++ tests/test_example_module_pytest.py
@@ -0,0 +1,40 @@
+"""Test of example package using pytest."""
+
+from pytest import fixture
+
+from examplepackage.example_module import Aclass
+
+
+@fixture
+def aclass():
+    """Return a class instance."""
+    return Aclass(first_variable=0)
+
+
+def test_set_a(aclass):
+    """Test a method."""
+    expected = 2
+
+    aclass.set_a(first_variable=expected)
+    result = aclass.first_variable
+
+    assert expected == result
+
+
+def test_set_b(aclass):
+    """Test a different method."""
+    expected = 2
+
+    aclass.set_b(second_variable=expected)
+    result = aclass.second_variable
+
+    assert expected == result
+
+
+def test_add(aclass):
+    """Test doing addition."""
+    expected = 2
+
+    result = aclass.add()
+
+    assert expected == result
